decb4d56c46e963899402468153bb5a1250000a4
Ashu <100476462@alumnos.uc3m.es>
2023-10-26 17:07:25 +0200

    l1b-26-10

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -5,8 +5,8 @@ from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\EarthObs\work\auxiliary'
-indir = r'C:\EODP_TER_2021\EODP-TS-ISM\input\gradient_alt100_act150' # small scene
-outdir = r'C:\EODP_TER_2021\EODP-TS-ISM\myoutput'
+indir = r'C:\EODP_TER_2021\EODP-TS-E2E\sgm_out' # small scene
+outdir = r'C:\EODP_TER_2021\EODP-TS-E2E\myoutput_E2E'
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
