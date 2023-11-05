87c48a92580d78a667c2c7b055b564ad40781eb1
Ashu <100476462@alumnos.uc3m.es>
2023-11-05 19:29:56 +0100

    ism_test-5-11

--- ism/test_ism/test_ism.py
+++ ism/test_ism/test_ism.py
@@ -10,21 +10,21 @@ outdir = r'C:\EODP_TER_2021\EODP-TS-ISM\myoutput' #My Output
 tol = 0.01e-2
 three_sigma = 1-0.997
 
-print('Test 1: Optical Module')
+print('Test Optical Module')
 print('---------------------')
 
-name1_isrf = []
-name1 = []
+namee_isrf = []
+namee = []
 for i in range(len(bands)):
-    name1.append('ism_toa_' + 'optical_' + str(bands[i]) + '.nc')
-    name1_isrf.append('ism_toa_' + 'isrf_' + str(bands[i]) + '.nc')
+    namee.append('ism_toa_'+'optical_'+str(bands[i])+'.nc')
+    namee_isrf.append('ism_toa_'+'isrf_'+str(bands[i])+'.nc')
 
 
-for val in range(len(bands)):
-    toa_ism = readToa(outdir, name1[val])
-    toa_ism_input = readToa(reference, name1[val])
-    toa_isrf_ism = readToa(outdir, name1_isrf[val])
-    toa_isrf_ism_input = readToa(reference, name1_isrf[val])
+for inn in range(len(bands)):
+    toa_ism = readToa(outdir,namee[inn])
+    toa_ism_input = readToa(reference,namee[inn])
+    toa_isrf_ism = readToa(outdir,namee_isrf[inn])
+    toa_isrf_ism_input = readToa(reference,namee_isrf[inn])
 
     result =np.zeros((toa_ism.shape[0],toa_ism.shape[1]))
     result_isrf =np.zeros((toa_isrf_ism.shape[0],toa_isrf_ism.shape[1]))
@@ -33,29 +33,29 @@ for val in range(len(bands)):
     counter =0
     for i in range(toa_ism.shape[0]):
         for j in range(toa_ism.shape[1]):
-            result[i,j] = toa_ism[i,j]-toa_ism_input[i, j]
-            result_isrf[i,j] = toa_isrf_ism[i, j]-toa_isrf_ism_input[i, j]
+            result[i,j]=toa_ism[i,j]-toa_ism_input[i,j]
+            result_isrf[i,j] = toa_isrf_ism[i,j]-toa_isrf_ism_input[i,j]
 
             if np.abs(result[i,j]>tol):
                 counter += 1
             if np.abs(result_isrf[i,j]>tol):
                 counter_isrf+= 1
 
-    points_threshold = toa_ism.shape[0] * toa_ism.shape[1] * three_sigma
+    points_treshold = toa_ism.shape[0]*toa_ism.shape[1]*three_sigma
 
     print('---------------------')
-    if counter < points_threshold:
-        print('Test ' + name1[val] + ' OK')
+    if counter < points_treshold:
+        print('Test ' + namee[inn] + ' OK')
     else:
-        print('Test ' + name1[val] + ' NOK')
+        print('Test ' + namee[inn] + ' NOK')
 
-    if counter_isrf < points_threshold:
-        print('Test ' + name1_isrf[val] + ' OK')
+    if counter_isrf < points_treshold:
+        print('Test ' + namee_isrf[inn] + ' OK')
     else:
-        print('Test ' + name1_isrf[val] + ' NOK')
+        print('Test ' + namee_isrf[inn] + ' NOK')
     print('---------------------')
 
-print('Test 2: Detection Module')
+print('Test Detection Module')
 print('---------------------')
 
 name_e = []
@@ -72,17 +72,17 @@ if len(name_e) != len(name_detection) != len(name_ds) != len(name_prnu):
     print('Error with the size of the toa in the Detection Module')
 
 
-for val in range(len(name_e)):
+for inn in range(len(name_e)):
 
-    toa_ism_ds = readToa(outdir, name_ds[val])
-    toa_ism_detection= readToa(outdir, name_detection[val])
-    toa_ism_e = readToa(outdir, name_e[val])
-    toa_ism_prnu = readToa(outdir, name_prnu[val])
+    toa_ism_ds = readToa(outdir,name_ds[inn])
+    toa_ism_detection= readToa(outdir,name_detection[inn])
+    toa_ism_e = readToa(outdir,name_e[inn])
+    toa_ism_prnu = readToa(outdir,name_prnu[inn])
 
-    toa_ism_e_input = readToa(reference, name_e[val])
-    toa_ism_ds_input = readToa(reference, name_ds[val])
-    toa_ism_detection_input= readToa(reference, name_detection[val])
-    toa_ism_prnu_input = readToa(reference, name_prnu[val])
+    toa_ism_e_input = readToa(reference,name_e[inn])
+    toa_ism_ds_input = readToa(reference,name_ds[inn])
+    toa_ism_detection_input= readToa(reference,name_detection[inn])
+    toa_ism_prnu_input = readToa(reference,name_prnu[inn])
 
     result_e =np.zeros((toa_ism_e.shape[0],toa_ism_e.shape[1]))
     result_ds =np.zeros((toa_ism_ds.shape[0],toa_ism_ds.shape[1]))
@@ -113,23 +113,23 @@ for val in range(len(name_e)):
                 counter_prnu += 1
 
     print('---------------------')
-    if counter_e < points_threshold:
-        print('Test ' + name_e[val] + ' OK')
+    if counter_e < points_treshold:
+        print('Test ' + name_e[inn] + ' OK')
     else:
-        print('Test ' + name_e[val] + ' NOK')
+        print('Test ' + name_e[inn] + ' NOK')
 
-    if counter_ds < points_threshold:
-        print('Test ' + name_ds[val] + ' OK')
+    if counter_ds < points_treshold:
+        print('Test ' + name_ds[inn] + ' OK')
     else:
-        print('Test ' + name_ds[val] + ' NOK')
+        print('Test ' + name_ds[inn] + ' NOK')
 
-    if counter_detection < points_threshold:
-        print('Test ' + name_detection[val] + ' OK')
+    if counter_detection < points_treshold:
+        print('Test ' + name_detection[inn] + ' OK')
     else:
-        print('Test ' + name_detection[val] + ' NOK')
+        print('Test ' + name_detection[inn] + ' NOK')
 
-    if counter_prnu < points_threshold:
-        print('Test ' + name_prnu[val] + ' OK')
+    if counter_prnu < points_treshold:
+        print('Test ' + name_prnu[inn] + ' OK')
     else:
-        print('Test ' + name_prnu[val] + ' NOK')
+        print('Test ' + name_prnu[inn] + ' NOK')
     print('---------------------')
